2c6f0914c88cc914b6fd6b990ae592c84e07af8e
kubotama <16697573+kubotama@users.noreply.github.com>
2025-04-10 23:01:58 +0900

    feat: 開始ボタンをクリックすると停止ボタンが表示されることを確認した
    ここでは、タイマーの時間が変わることを確認するのは難しいので、停止ボタンが表示されることだけを確認した。

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
+
 const App: React.FC = () => {
   const timerSecond = 180;
+  const [buttonLabel, setButtonLabel] = useState("開始");
+
+  const handleStartClick = () => {
+    setButtonLabel("停止");
+  };
 
   return (
     <div>
-      <button style={{ width: "200px" }}> 開始</button>
+      <button style={{ width: "200px" }} onClick={handleStartClick}>
+        {buttonLabel}
+      </button>
       <span aria-label="time-text">
         {timerSecond.toString().padStart(3, "0")}
       </span>
