b5bdc70405e6ca5d150fa950899c4f40004dc35a
kubotama <16697573+kubotama@users.noreply.github.com>
2025-04-16 22:27:25 +0900

    test: UIにあわせて、テストを修正した

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,7 +9,7 @@ import App from "./App";
 describe("App Component", () => {
   it("renders the heading", () => {
     render(<App />);
-    const headingElement = screen.getByText(/Hello from Chrome Extension!/i);
+    const headingElement = screen.getByText(/開始/i);
     expect(headingElement).toBeInTheDocument();
   });
 });
