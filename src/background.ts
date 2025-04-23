32a43353f416df6feafc62adc860877cac66c6f2
kubotama <16697573+kubotama@users.noreply.github.com>
2025-04-23 14:53:34 +0900

    update: タイマーの時間の測定が正確になるように修正した。
    これまでは、1秒ごとにコールバックを呼び出すタイミングで1秒経過したことにしていた。これでは、なんらかの理由で呼び出しが遅れるとタイマーの時間がずれてしまう。
    タイマーの開始時点で、終了の時間を計算しておいて、それとの比較で終了したかを判断するように修正した。

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,13 @@
+const TIMER_LABEL = "timer";
 type TimerState = "isRunning" | "isPaused";
 let timerState: TimerState = "isPaused";
 const timerDuration = 180; // 3分
+let endTimeMillisec = 0;
 
 chrome.action.onClicked.addListener(() => {
   if (timerState === "isPaused") {
-    timerState = "isRunning";
     handleStartTimer(timerDuration); // 3分
   } else {
-    timerState = "isPaused";
     handleStopTimer();
   }
 });
@@ -20,51 +20,60 @@ chrome.runtime.onStartup.addListener(() => {
 });
 
 // 拡張機能がインストールされたときの処理
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Timer Extension installed");
-  // 初期設定を保存
-  chrome.storage.local.set({
-    isRunning: false,
-    timeLeft: timerDuration, // デフォルトの時間（秒）
-  });
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   console.log("Timer Extension installed");
+//   // 初期設定を保存
+//   chrome.storage.local.set({
+//     // isRunning: false,
+//     timeLeft: timerDuration, // デフォルトの時間（秒）
+//   });
+// });
 
 // タイマー開始処理
 const handleStartTimer = (duration: number): void => {
-  chrome.storage.local.set({ isRunning: true, timeLeft: duration });
+  // chrome.storage.local.set({ isRunning: true, timeLeft: duration });
+  // chrome.storage.local.set({ timeLeft: duration });
+  // アラームを設定
+  if (timerState !== "isRunning") {
+    chrome.alarms.create(TIMER_LABEL, {
+      periodInMinutes: 1 / 60, // 1秒ごとに更新
+    });
+  }
+  endTimeMillisec = new Date().getTime() + duration * 1000;
   chrome.action.setBadgeText({ text: duration.toString() });
   chrome.action.setBadgeBackgroundColor({ color: "#4CAF50" });
-
-  // アラームを設定
-  chrome.alarms.create("timer", {
-    periodInMinutes: 1 / 60, // 1秒ごとに更新
-  });
+  timerState = "isRunning";
 };
 
 // タイマー停止処理
 const handleStopTimer = (): void => {
-  chrome.storage.local.set({ isRunning: false });
+  // chrome.storage.local.set({ isRunning: false });
   chrome.action.setBadgeText({ text: timerDuration.toString() });
   chrome.action.setBadgeBackgroundColor({ color: "#F44336" });
-  chrome.alarms.clear("timer");
+  chrome.alarms.clear(TIMER_LABEL);
+  timerState = "isPaused";
 };
 
 // アラームリスナー
 chrome.alarms.onAlarm.addListener(async (alarm) => {
-  if (alarm.name === "timer") {
-    const data = await chrome.storage.local.get(["timeLeft", "isRunning"]);
-    if (data.isRunning && data.timeLeft > 0) {
-      const newTime = data.timeLeft - 1;
-      await chrome.storage.local.set({ timeLeft: newTime });
-      chrome.action.setBadgeText({ text: newTime.toString() });
-
-      if (newTime <= 0) {
-        handleStartTimer(timerDuration); // タイマーをリセット
-        await createOffscreen();
-        chrome.runtime.sendMessage({
-          play: "",
-        });
-      }
+  if (alarm.name === TIMER_LABEL) {
+    // const data = await chrome.storage.local.get(["timeLeft", "isRunning"]);
+    // const data = await chrome.storage.local.get(["timeLeft"]);
+    const nowMillisec = new Date().getTime();
+    const difference = endTimeMillisec - nowMillisec;
+    const remainingMillisec = Math.max(0, difference); // 残り時間（ミリ秒）
+    if (timerState === "isRunning" && remainingMillisec > 0) {
+      // const newTime = data.timeLeft - 1;
+      // await chrome.storage.local.set({ timeLeft: newTime });
+      chrome.action.setBadgeText({
+        text: Math.round(remainingMillisec / 1000).toString(),
+      });
+    } else if (remainingMillisec <= 0) {
+      handleStartTimer(timerDuration); // タイマーをリセット
+      await createOffscreen();
+      chrome.runtime.sendMessage({
+        play: "",
+      });
     }
   }
 });
